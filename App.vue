c320aec99141f5c9850956a186be69d2d7b1e8a5
haozyx <826060882@qq.com>
2019-10-25 12:24:05 +0800

    tc init

---
+++ App.vue
@@ -0,0 +1,45 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	/*每个页面公共css */
+	.page {
+		width: 100%;
+		height: 100%;
+		background-color: #f7f7f7;
+		/* 	position: absolute; */
+	}
+	.page-fill {
+		width: 100%;
+		height: 100%;
+		background-color: #f7f7f7;
+		position: absolute;
+	}
+	/* 线条 */
+	.line-wrap {
+		padding: 0upx 35upx;
+	}
+	.line {
+		height: 1px;
+		background-color: #e3e3e3;
+	}
+	/* 这是隐藏滚动条的，在单个页面设置不管用 */
+	::-webkit-scrollbar {
+		display: none;
+	}
+	.bcolor{
+		background-color: #52A9EC;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,52 @@
+<template>
+	<view class="page">
+		 <!-- 搜索框 start-->
+			<view class="searchwrap bcolor">
+				<view class="bodysearch">
+					<image src="../../static/img/index/search.png" class="searchimg"></image>
+					<input class="searchtext" type="text" maxlength="20" placeholder="找工作|搜房子" placeholder-style="searchhint"/>
+					<view class="searchbtn">
+						搜索
+					</view>
+				</view>
+				<view class="bottomview">
+					本站浏览1205.34万  发布总量:5600
+				</view>
+			</view>
+		 <!-- 搜索框 end-->
+		 
+		 <!-- 信息分类 start-->
+		 
+		 <!-- 信息分类 end-->
+		 
+		 <!-- 轮播广告位  目前不加-->
+		 
+		 <!-- 分类的导航栏 start-->
+		 
+		 <!-- 分类的导航栏 end-->
+		 
+		 <!-- 列表详情 start-->
+		 
+		 <!-- 列表详情 end-->
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	@import url("index.css");
+</style>
